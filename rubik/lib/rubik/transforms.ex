71a98d318234551c3395accad79c1e8132221ea9
Timothée Behra <tbehra42@gmail.com>
2021-04-23 15:18:27 +0200

    Corner transformations

--- rubik/lib/rubik/transforms.ex
+++ rubik/lib/rubik/transforms.ex
@@ -1,17 +1,74 @@
 defmodule Rubik.Transforms do
   
-  def apply(cube, "R") do
-    #urf, bru, drb, frd 
-    %Rubik.State{ cube |
-      URB: rotate_corner(cube."URF"),
-      DRB: rotate_corner(cube."URB"),
-      DRF: rotate_corner(cube."DRB"),
-      URF: rotate_corner(cube."DRF")
-    }
-  end 
-
-  defp rotate_corner(corner) do
-    str = Atom.to_string(corner)
-    String.to_atom(String.at(str, 2) <> String.at(str, 1) <> String.at(str, 0))
+  defp corner_to_corner([h | t] = corner_list, _direction = :next) do
+    Enum.zip(corner_list, t ++ [h])
   end
+
+  defp corner_to_corner(corner_list, _direction = :previous) do
+    Enum.zip(corner_list, [List.last(corner_list)] ++ corner_list)
+  end
+
+  defp corner_transformation(cube, rotation, corner_tuples) do
+    Enum.reduce(corner_tuples, cube,
+      fn ({from, to}, acc_cube) -> 
+        Map.update!(acc_cube, from,
+          fn _val -> rotate_corner(Map.get(cube, to), rotation) end) 
+      end
+    )
+  end
+
+  defp rotate_corner(corner, [first, second, third]) do
+    String.at(corner, first)
+      <> String.at(corner, second)
+      <> String.at(corner, third)
+  end
+
+  def apply(cube, "F") do
+    corner_transformation(
+      cube,
+      [1, 0, 2],
+      corner_to_corner([:ULF, :URF, :DRF, :DLF], :previous)
+    )
+  end
+
+  def apply(cube, "B") do
+    corner_transformation(
+      cube,
+      [1, 0, 2],
+      corner_to_corner([:ULB, :URB, :DRB, :DLB], :next)
+    )
+  end
+
+  def apply(cube, "U") do
+    corner_transformation(
+      cube,
+      [0, 2, 1],
+      corner_to_corner([:ULB, :URB, :URF, :ULF], :previous)
+    )
+  end
+
+  def apply(cube, "D") do
+    corner_transformation(
+      cube,
+      [0, 2, 1],
+      corner_to_corner([:DLB, :DRB, :DRF, :DLF], :next)
+    )
+  end
+
+  def apply(cube, "L") do
+    corner_transformation(
+      cube,
+      [2, 1, 0],
+      corner_to_corner([:ULB, :DLB, :DLF, :ULF], :next)
+    )
+  end
+
+  def apply(cube, "R") do
+    corner_transformation(
+      cube,
+      [2, 1, 0],
+      corner_to_corner([:URF, :URB, :DRB, :DRF], :previous)
+    )
+  end
+
 end
--- rubik/test/rubik_test.exs
+++ rubik/test/rubik_test.exs
@@ -4,16 +4,32 @@ defmodule RubikTest do
   alias Rubik.Cube
   alias Rubik.Transforms
 
-  test "4 identical q-turns get back to initial state" do
+  test "Applying the left transformation on a solved cube" do
     cube = Cube.new_cube()
-    c1 = Transforms.apply(cube, "R") 
-    assert c1 != cube
-    c2 = Transforms.apply(c1, "R") 
-    assert c2 != c1
-    c3 = Transforms.apply(c2, "R") 
-    assert c3 != c2
-    c4 = Transforms.apply(c3, "R") 
-    assert c4 == cube
+    c1 = Transforms.apply(cube, "L")
+    assert c1."ULF" == "blu"
+    assert c1."ULB" == "bld"
+    assert c1."DLB" == "fld"
+    assert c1."DLF" == "flu"
+    c2 = Transforms.apply(c1, "L")
+    assert c2."ULF" == "dlb"
+    assert c2."ULB" == "dlf"
+    assert c2."DLB" == "ulf"
+    assert c2."DLF" == "ulb"
+  end
+
+  test "4 identical q-turns get back to initial state" do
+    for transform <- ["F", "L", "U", "R", "D", "B"] do 
+      cube = Cube.new_cube()
+      c1 = Transforms.apply(cube, transform) 
+      assert c1 != cube
+      c2 = Transforms.apply(c1, transform) 
+      assert c2 != c1
+      c3 = Transforms.apply(c2, transform) 
+      assert c3 != c2
+      c4 = Transforms.apply(c3, transform) 
+      assert c4 == cube
+    end
   end
 
 end
