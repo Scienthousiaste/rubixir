583e362ea8cfca9090d646cd96fdeff296be1d61
Timothée Behra <tbehra42@gmail.com>
2021-04-23 17:29:42 +0200

    All basic transformations corners and edges, with a decent test passing

--- rubik/lib/rubik/transforms.ex
+++ rubik/lib/rubik/transforms.ex
@@ -1,20 +1,11 @@
 defmodule Rubik.Transforms do
   
   defp corner_to_corner([h | t] = corner_list, _direction = :next) do
-    Enum.zip(corner_list, t ++ [h])
+    Enum.zip(t ++ [h], corner_list)
   end
 
   defp corner_to_corner(corner_list, _direction = :previous) do
-    Enum.zip(corner_list, [List.last(corner_list)] ++ corner_list)
-  end
-
-  defp corner_transformation(cube, rotation, corner_tuples) do
-    Enum.reduce(corner_tuples, cube,
-      fn ({from, to}, acc_cube) -> 
-        Map.update!(acc_cube, from,
-          fn _val -> rotate_corner(Map.get(cube, to), rotation) end) 
-      end
-    )
+    Enum.zip([List.last(corner_list)] ++ corner_list, corner_list)
   end
 
   defp rotate_corner(corner, [first, second, third]) do
@@ -23,52 +14,88 @@ defmodule Rubik.Transforms do
       <> String.at(corner, third)
   end
 
+  defp corner_transformation(cube, rotation, corner_tuples) do
+    Enum.reduce(corner_tuples, cube,
+      fn ({from, to}, acc_cube) -> 
+        Map.update!(acc_cube, to,
+          fn _val -> rotate_corner(Map.get(cube, from), rotation) end) 
+      end
+    )
+  end
+
+  defp edge_to_edge([h | t] = edges) do
+    Enum.zip(edges, t ++ [h]) 
+  end
+
+  defp may_switch_edge(edge, :switch) do
+    String.at(edge, 1) <> String.at(edge, 0) 
+  end
+  defp may_switch_edge(edge, :no_switch) do
+    edge
+  end
+
+  defp edge_transformation(cube, edges, switch) do
+    edge_to_edge(edges)
+    |> Enum.reduce(cube, 
+      fn ({from, to}, acc_cube) ->
+        Map.update!(acc_cube, to,
+          fn _val -> may_switch_edge(Map.get(cube, from), switch) end)
+      end
+    )
+  end
+
   def apply(cube, "F") do
-    corner_transformation(
-      cube,
+    cube
+    |> corner_transformation(
       [1, 0, 2],
       corner_to_corner([:ULF, :URF, :DRF, :DLF], :previous)
     )
+    |> edge_transformation([:UF, :RF, :DF, :LF], :no_switch)
   end
 
   def apply(cube, "B") do
-    corner_transformation(
-      cube,
+    cube
+    |> corner_transformation(
       [1, 0, 2],
       corner_to_corner([:ULB, :URB, :DRB, :DLB], :next)
     )
+    |> edge_transformation([:UB, :LB, :DB, :RB], :no_switch)
   end
 
   def apply(cube, "U") do
-    corner_transformation(
-      cube,
+    cube
+    |> corner_transformation(
       [0, 2, 1],
       corner_to_corner([:ULB, :URB, :URF, :ULF], :previous)
     )
+    |> edge_transformation([:UB, :UR, :UF, :UL], :no_switch)
   end
 
   def apply(cube, "D") do
-    corner_transformation(
-      cube,
+    cube
+    |> corner_transformation(
       [0, 2, 1],
       corner_to_corner([:DLB, :DRB, :DRF, :DLF], :next)
     )
+    |> edge_transformation([:DF, :DR, :DB, :DL], :no_switch)
   end
 
   def apply(cube, "L") do
-    corner_transformation(
-      cube,
+    cube
+    |> corner_transformation(
       [2, 1, 0],
       corner_to_corner([:ULB, :DLB, :DLF, :ULF], :next)
     )
+    |> edge_transformation([:UL, :LF, :DL, :LB], :switch)
   end
 
   def apply(cube, "R") do
-    corner_transformation(
-      cube,
+    cube
+    |> corner_transformation(
       [2, 1, 0],
       corner_to_corner([:URF, :URB, :DRB, :DRF], :previous)
     )
+    |> edge_transformation([:UR, :RB, :DR, :RF], :switch)
   end
 
 end
--- rubik/test/rubik_test.exs
+++ rubik/test/rubik_test.exs
@@ -8,14 +8,13 @@ defmodule RubikTest do
     cube = Cube.new_cube()
     c1 = Transforms.apply(cube, "L")
     assert c1."ULF" == "blu"
+    assert c1."LF"  == "lu"
     assert c1."ULB" == "bld"
+    assert c1."DL"  == "fl"
     assert c1."DLB" == "fld"
+    assert c1."LB"  == "ld"
     assert c1."DLF" == "flu"
-    c2 = Transforms.apply(c1, "L")
-    assert c2."ULF" == "dlb"
-    assert c2."ULB" == "dlf"
-    assert c2."DLB" == "ulf"
-    assert c2."DLF" == "ulb"
+    assert c1."UL"  == "bl"
   end
 
   test "4 identical q-turns get back to initial state" do
@@ -30,6 +29,35 @@ defmodule RubikTest do
       c4 = Transforms.apply(c3, transform) 
       assert c4 == cube
     end
+  end
+
+  test "Applying all basic transformations and get the correct state of the cube" do
+    cube = Enum.reduce(["L", "U", "L", "D", "F", "R", "B", "R"], Cube.new_cube(),
+      fn transformation, cube -> Rubik.Transforms.apply(cube, transformation) end
+    )
+
+    assert cube == %Rubik.State{
+      URF: "dfr",
+      URB: "bur", 
+      ULB: "lbu", 
+      ULF: "ful", 
+      DRF: "rdb",
+      DRB: "bld", 
+      DLB: "ldf", 
+      DLF: "urf", 
+      UR:   "df",
+      UB:   "bu",
+      UL:   "dl",
+      UF:   "fu",
+      DR:   "rd",
+      DB:   "lf",
+      DL:   "db",
+      DF:   "rf",
+      LF:   "ul",
+      LB:   "bl",
+      RF:   "rb",
+      RB:   "ur"
+    }
   end
 
 end
