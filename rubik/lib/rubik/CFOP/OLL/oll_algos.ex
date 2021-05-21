d02926891dcebe1be6df8c6987d8cac420e22a75
Timothée Behra <tbehra42@gmail.com>
2021-05-21 18:29:53 +0200

    Five next oll algos

--- rubik/lib/rubik/CFOP/OLL/oll_algos.ex
+++ rubik/lib/rubik/CFOP/OLL/oll_algos.ex
@@ -13,8 +13,17 @@ defmodule Rubik.OLL.Algorithms do
   
   def get_oll_algos() do
     [
-      make_oll_algo("LLRRBRFL", ["R", "U", "B'", "R", "B", "R2", "U'", "R'", "F", "R", "F'"])
+      make_oll_algo("LLRRBRFL", ["R", "U", "B'", "R", "B", "R2", "U'", "R'", "F", "R", "F'"]),
+
+      make_oll_algo("FLRFBRFL", ["R'", "F", "R", "F'", "U2", "R'", "F", "R", "F2", "U2", "F"]),
+      make_oll_algo("LBRUBRFL", ["F'", "B2", "L", "B'", "L", "F", "U2", "F'", "L", "B'", "F"]),
+
+      make_oll_algo("FLURBRFL", ["R'", "U2", "R'", "F", "R", "F'", "U'", "F'", "U'", "F", "U'", "R"]),
+      make_oll_algo("LUBUBRFL", ["R", "U", "R'", "U", "R'", "F", "R", "F'", "U2", "R'", "F", "R", "F'"]),
+      make_oll_algo("UUUUBRFL", ["L", "R'", "F2", "L'", "R", "U2", "L", "R'", "F", "L'", "R", "U2", "L", "R'", "F2", "L'", "R"]),
+
     ]
+
   end
 
   def get_oll_algo_map(face = :D) do
