77ec78ecae9f53e6a72df62e8a86d68b2a05ff3e
Timothée Behra <tbehra.dev@gmail.com>
2025-10-29 11:55:54 +0100

    updating deps, removing warnings

--- visu/lib/visu_web/gettext.ex
+++ visu/lib/visu_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule VisuWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :visu
+  use Gettext.Backend, otp_app: :visu
 end
--- visu/mix.exs
+++ visu/mix.exs
@@ -7,7 +7,7 @@ defmodule Visu.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -32,7 +32,7 @@ defmodule Visu.MixProject do
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
+      {:gettext, "~> 0.26"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
 
